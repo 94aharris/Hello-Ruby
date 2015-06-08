27c11644ab9c4431ef16d7a185411b18d0eb7077
Anthony <94aharris@gmail.com>
2015-06-08 14:42:08 -0400

    Update RubyCalc.rb
    Added more documentation

--- RubyCalc.rb
+++ RubyCalc.rb
@@ -1,3 +1,5 @@
-#This is a simple command line calculator written in Ruby. 
-
+## This is a simple command line calculator written in Ruby. 
+## Functions which I intend to include:
+## Addition, Subtraction, Multiplication, Division, Factorals, "Add from 1 to num", Modulation, Prime Number Tester
+## "Which Number is Greater", "Order of Operations Helper"
 puts("Welcome To The Ruby Calculator!")
